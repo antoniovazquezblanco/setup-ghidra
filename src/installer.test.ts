b76ff3d4d49d33187c56dcae696e1fb357e81d21
Antonio Vázquez Blanco <antoniovazquezblanco@gmail.com>
2024-06-24 22:07:14 +0200

    Update octokit

--- src/installer.test.ts
+++ src/installer.test.ts
@@ -1,17 +1,19 @@
-import path = require("path");
-import io = require("@actions/io");
+import { fileURLToPath } from "url";
+import * as path from "path";
+import * as io from "@actions/io";
 import * as github_helper from "./github_helper.js";
 import * as installer from "./installer.js";
 import * as fs from "fs";
 
 describe("Installer tests", () => {
+  const sourceDir = path.dirname(fileURLToPath(import.meta.url));
   let url = "";
   let sha256sum = "";
 
   beforeAll(async () => {
     // Modify process environment to include the needed variables to run...
-    process.env["RUNNER_TOOL_CACHE"] = path.join(__dirname, "runner", "tools");
-    process.env["RUNNER_TEMP"] = path.join(__dirname, "runner", "temp");
+    process.env["RUNNER_TOOL_CACHE"] = path.join(sourceDir, "runner", "tools");
+    process.env["RUNNER_TEMP"] = path.join(sourceDir, "runner", "temp");
 
     // Get a valid info for installation...
     [url, sha256sum] = await github_helper.getReleaseInfo(
@@ -23,7 +25,7 @@ describe("Installer tests", () => {
 
   afterAll(async () => {
     // After the tests, remove the runner directory
-    await io.rmRF(path.join(__dirname, "runner"));
+    await io.rmRF(path.join(sourceDir, "runner"));
   });
 
   it(
