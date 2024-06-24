165d24097ac38e93eb442cfd36991b99fbbcb8f9
Antonio Vázquez Blanco <antoniovazquezblanco@gmail.com>
2024-06-24 21:31:29 +0200

    Format fix

--- jest.config.ts
+++ jest.config.ts
@@ -8,12 +8,12 @@ import type { Config } from "jest";
 const config: Config = {
   preset: "ts-jest/presets/default-esm",
   moduleNameMapper: {
-    '^(\\.{1,2}/.*)\\.js$': '$1',
+    "^(\\.{1,2}/.*)\\.js$": "$1",
   },
   verbose: true,
   collectCoverage: true,
   coverageDirectory: "coverage",
-  coverageProvider: "v8"
+  coverageProvider: "v8",
 };
 
 export default config;
